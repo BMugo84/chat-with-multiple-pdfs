5ef106b064bafe9b6c0fa3a27ea82a01dd8782f1
BMugo84 <mugo.benson.kbm@gmail.com>
2025-01-01 17:13:22 -0600

    made a conversation chain to interact with the user using llm

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 # from langchain.vectorstores import FAISS
 from langchain_community.vectorstores import FAISS
+from langchain_community.llms import HuggingFaceHub
+from langchain.memory import ConversationBufferMemory
+from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -32,10 +35,27 @@ def get_vectorstore(text_chunks):
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
+def get_conversation_chain(vectorstore):
+    llm = HuggingFaceHub(
+        repo_id="mistralai/Mistral-7B-Instruct-v0.1",
+        model_kwargs={"temperature": 0.7, "max_length": 512}
+    )
+    memory = ConversationBufferMemory(memory_key='chat_history',
+                                      return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
 
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation=None
 
     st.header("Chat with multiple PDFs :books:")
     st.text_input("Ask a question about your documents:")
@@ -54,8 +74,11 @@ def main():
                 # st.write(text_chunks)
 
                 # create vector store 
-                vector_store = get_vectorstore(text_chunks)
+                vectorstore = get_vectorstore(text_chunks)
                 st.write("Processing complete!")
+
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(vectorstore)
 
 
 
